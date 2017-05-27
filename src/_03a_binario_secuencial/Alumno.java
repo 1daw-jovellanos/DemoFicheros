974eb435779b4de55585b1905fb83ff6d99f1214
vjfernandez <victor.fernandezgil@educa.madrid.org>
2017-05-28 00:45:25 +0200

    Con acceso aleatorio

---
+++ src/_03a_binario_secuencial/Alumno.java
@@ -0,0 +1,21 @@
+package _03a_binario_secuencial;
+
+
+public class Alumno {
+    private String nombre;
+    private double nota;
+
+    public Alumno(String nombre, double nota) {
+        this.nombre = nombre;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+    
+}
